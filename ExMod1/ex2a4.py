d5181ad9d6a94c80bb6622ad86c8e842919c80f2
Jyun <gustavohayashida00@gmail.com>
2021-04-25 16:11:39 -0300

    Melhor organização

---
+++ ExMod1/ex2a4.py
@@ -0,0 +1,5 @@
+Dia = input('Dia =')
+Mês = input('Mês =')
+Ano = input('Ano =')
+
+print('Você nasceu no dia ' + Dia + ' de ' + Mês + ' de ' + Ano + '. Correto?')
---
+++ ExMod1/ex3a4.py
@@ -0,0 +1,8 @@
+n1 = int(input('Primeiro número = '))
+n2 = int(input('Segundo número = '))
+
+soma = n1 + n2
+
+
+print('A soma dos dois números é', (soma))
+print('A soma vale {}'.format(soma))
