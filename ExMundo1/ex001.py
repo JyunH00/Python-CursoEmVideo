f4554281df96cd1effe508afad5788e46e4da99f
Jyun <gustavohayashida00@gmail.com>
2021-04-27 17:58:37 -0300

    Pequenas mudanças

---
+++ ExMundo1/ex001.py
@@ -0,0 +1,12 @@
+'''
+Exercício 1: Escreva uma mensagem que diga: "Olá, mundo!"
+'''
+
+##### Resolução #####
+
+### Criação das variáveis ###
+
+msg = 'Olá, mundo!'
+
+### Print do resultado ###
+print(msg)
---
+++ ExMundo1/ex002.py
@@ -0,0 +1,22 @@
+'''
+Exercício 2: Escreva um programa que pergunte seu nome e te cumprimente
+'''
+
+##### Resolução #####
+
+### Criação das variáveis ###
+
+nome = input('Digite seu nome: ')
+
+### Print do resultado ###
+
+print('É um prazer te conhecer ' + nome + '!')
+
+
+### Outras resoluções de print ###
+'''
+print('É um prazer te conhecer', nome + '!')
+
+print('É um prazer te conhecer, {}!'.format(nome))
+
+'''
